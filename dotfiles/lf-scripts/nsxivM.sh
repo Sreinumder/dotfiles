4fbb7654a513529827e04bc122383dea81e4642f
Sreinumder <rojgosai02@gmail.com>
2024-03-03 22:21:20 +0545

    made nsxiv recursively open images from a folder in case of dir

--- dotfiles/lf-scripts/nsxivM.sh
+++ dotfiles/lf-scripts/nsxivM.sh
@@ -2,17 +2,22 @@
 
 NAME=`echo "$1" | rev | cut -d "/" -f 1 | rev`
 
-# Checking if file is provided with just name or full path
+#if the file name does not contain any dot it is probably an directory
+if [[ ! $NAME = *.* ]]; then 
+  nsxiv -rqfi $1
+  exit 0
+fi
+
 LOC=`dirname "$1"`
 if [[ $LOC = *"/home/"* ]]; then
-	cd "$LOC"
+  cd "$LOC"
 fi
 
 n=0
 ls | while read p; do
-	n=$((n+1))
-	if [[ "$p" = "$NAME" ]]; then
-		ls | nsxiv -q -bin $n
-		break
-	fi
+  n=$((n+1))
+  if [[ "$p" = "$NAME" ]]; then
+    ls | nsxiv -qf -in $n
+    break
+  fi
 done
