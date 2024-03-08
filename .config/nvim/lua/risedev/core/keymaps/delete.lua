06ca09ecb0d6b6b4c3037549f17f6e21a2d6c07b
Sreinumder <rojgosai02@gmail.com>
2024-03-08 13:53:11 +0545

    colemak layer fix

--- .config/nvim/lua/risedev/core/keymaps/delete.lua
+++ .config/nvim/lua/risedev/core/keymaps/delete.lua
@@ -1,14 +1,14 @@
 local keymap = vim.keymap -- for conciseness
 
 -- delete with x d or D and cut with alt + x + d + D
-keymap.set({'n','v'}, "x", '"_x')
-keymap.set({'n','v'}, "d", '"_d')
-keymap.set({'n','v'}, "D", '"_D')
-keymap.set({'n','v'}, "c", '"_c')
-keymap.set({'n','v'}, "<A-d>", 'd')
-keymap.set({'n','v'}, "<A-d><A-d>", 'dd')
-keymap.set({'n','v'}, "<A-D>", 'D')
-keymap.set({'n','v'}, "<A-c>", 'c')
-keymap.set({'n','v'}, "<A-c><A-c>", 'cc')
+keymap.set({'n','v','x'}, "x", '"_x')
+keymap.set({'n','v','x'}, "d", '"_d')
+keymap.set({'n','v','x'}, "D", '"_D')
+keymap.set({'n','v','x'}, "c", '"_c')
+keymap.set({'n','v','x'}, "<A-d>", 'd')
+keymap.set({'n','v','x'}, "<A-d><A-d>", 'dd')
+keymap.set({'n','v','x'}, "<A-D>", 'D')
+keymap.set({'n','v','x'}, "<A-c>", 'c')
+keymap.set({'n','v','x'}, "<A-c><A-c>", 'cc')
 keymap.set({'n'}, "dH", '"_d^', {desc = "same as d^"})
 keymap.set({'n'}, "dL", '"_d$', {desc = "same as d$"})
