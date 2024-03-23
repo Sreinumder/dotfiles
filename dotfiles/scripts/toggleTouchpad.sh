8b6414ec0b0f88c30faeff7dc15effe5c77127cf
Sreinumder <rojgosai02@gmail.com>
2024-03-24 01:31:29 +0545

    fixed touchpad disable script

--- dotfiles/scripts/toggleTouchpad.sh
+++ dotfiles/scripts/toggleTouchpad.sh
@@ -1,24 +1,19 @@
-​#!/bin/sh
+#!/bin/bash
+touchpadConf="$HOME/dotfiles/hypr/conf/touchpad.conf"
 
-# Set device to be toggled
-HYPRLAND_DEVICE="elan-touchpad"
-HYPRLAND_VARIABLE="device:$HYPRLAND_DEVICE:enabled"
-
-if [ -z "$XDG_RUNTIME_DIR" ]; then
-	export XDG_RUNTIME_DIR=/run/user/$(id -u)
+if [ ! -f "$touchpadConf" ]; then
+  notify-send -u normal "Error: touchpad.conf not found at '$toucpadConf'"
+  exit 1
 fi
 
-# Check if device is currently enabled (1 = enabled, 0 = disabled)
-DEVICE="$(hyprctl getoption $HYPRLAND_VARIABLE | grep 'int: 1')"
-echo $DEVICE
-if [ -z "$DEVICE" ]; then
-	# if the device is disabled, then enable
-	notify-send -u normal "Enabling Touchpad"
-	hyprctl keyword $HYPRLAND_VARIABLE true
-	hyprctl keyword general:cursor_inactive_timeout 12
-else
-	# if the device is enabled, then disable
-	notify-send -u normal "Disabling Touchpad"
-	hyprctl keyword $HYPRLAND_VARIABLE false
+currentState=$(grep -P 'enabled =' "$touchpadConf" | awk '{print $NF}')
+if [[ "$currentState" == "true" ]]; then 
+  newState="false"; 
+  notify-send -u normal "touchpad disabled"
 	hyprctl keyword general:cursor_inactive_timeout 1
+else 
+  newState="true"; 
+  notify-send -u normal "touchpad enabled"
 fi
+
+sed -i "s/enabled = $currentState/enabled = $newState/" "$touchpadConf"
