aa8266a3564ac060680e95bd603c8201dae54cfd
Sreinumder <rojgosai02@gmail.com>
2024-04-02 19:15:34 +0545

    qute config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -10,8 +10,7 @@ config.bind('xt', 'config-cycle tabs.show always never')
 config.bind('xx', 'config-cycle tabs.show always never;; config-cycle statusbar.show always never')
 
 c.url.searchengines = {
-    'DEFAULT':  'https://google.com/search?hl=en&q={}',
-    '!a':       'https://www.amazon.com/s?k={}',
+    'DEFAULT':  'https://duckduckgo.com/search?hl=en&q={}',
     '!d':       'https://duckduckgo.com/?ia=web&q={}',
     '!dd':      'https://thefreedictionary.com/{}',
     '!e':       'https://www.ebay.com/sch/i.html?_nkw={}',
@@ -19,7 +18,6 @@ c.url.searchengines = {
     '!gh':      'https://github.com/search?o=desc&q={}&s=stars',
     '!gist':    'https://gist.github.com/search?q={}',
     '!gi':      'https://www.google.com/search?tbm=isch&q={}&tbs=imgo:1',
-    '!gn':      'https://news.google.com/search?q={}',
     '!ig':      'https://www.instagram.com/explore/tags/{}',
     '!m':       'https://www.google.com/maps/search/{}',
     '!p':       'https://pry.sh/{}',
