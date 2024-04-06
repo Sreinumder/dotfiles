ee48b38b7a99e4b204d8000fcca2e77e56439d2b
Sreinumder <rojgosai02@gmail.com>
2024-04-07 00:37:04 +0545

    qute

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -11,9 +11,9 @@ config.bind('xx', 'config-cycle tabs.show always never;; config-cycle statusbar.
 
 c.url.searchengines = {
     'DEFAULT':  'https://duckduckgo.com/?q={}&ia=web',
-    '!d':       'https://duckduckgo.com/?ia=web&q={}',
     '!dd':      'https://thefreedictionary.com/{}',
-    '!e':       'https://www.ebay.com/sch/i.html?_nkw={}',
+    '!cmk':     'https://comick.io/search?q={}',
+    '!ani':     'https://aniwave.to/filter?keyword={}',
     '!fb':      'https://www.facebook.com/s.php?q={}',
     '!gh':      'https://github.com/search?o=desc&q={}&s=stars',
     '!gist':    'https://gist.github.com/search?q={}',
@@ -22,11 +22,9 @@ c.url.searchengines = {
     '!m':       'https://www.google.com/maps/search/{}',
     '!p':       'https://pry.sh/{}',
     '!r':       'https://www.reddit.com/search?q={}',
-    '!sd':      'https://slickdeals.net/newsearch.php?q={}&searcharea=deals&searchin=first',
     '!t':       'https://www.thesaurus.com/browse/{}',
     '!tw':      'https://twitter.com/search?q={}',
     '!mal':     'https://myanimelist.net/search/all?q={}&cat=all',
     '!w':       'https://en.wikipedia.org/wiki/{}',
-    '!yelp':    'https://www.yelp.com/search?find_desc={}',
     '!yt':      'https://www.youtube.com/results?search_query={}'
 }
