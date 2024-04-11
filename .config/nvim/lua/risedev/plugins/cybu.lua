644916fb4ed00525f723fa34e973195f1a594946
Sreinumder <rojgosai02@gmail.com>
2024-04-11 16:32:25 +0545

    plugin changes

--- .config/nvim/lua/risedev/plugins/cybu.lua
+++ .config/nvim/lua/risedev/plugins/cybu.lua
@@ -1,9 +1,8 @@
 return {
     "ghillb/cybu.nvim",
-    
     branch = "main", -- timely updates
     -- branch = "v1.x", -- won't receive breaking changes
-    requires = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim"}, -- optional for icon support
+    requires = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" }, -- optional for icon support
     config = function()
         require('cybu').setup()
         local ok, cybu = pcall(require, "cybu")
@@ -11,8 +10,8 @@ return {
             return
         end
         cybu.setup()
-        vim.keymap.set("n",  "<c-s-tab>","<Plug>(CybuPrev)")
-        vim.keymap.set("n",  "<c-tab>", "<Plug>(CybuNext)")
+        vim.keymap.set("n", "<C-S-k>", "<Plug>(CybuPrev)")
+        vim.keymap.set("n",  "<C-S-j>", "<Plug>(CybuNext)")
         -- vim.keymap.set({"n", "v"}, "<c-s-tab>", "<plug>(CybuLastusedPrev)")
         -- vim.keymap.set({"n", "v"}, "<c-tab>", "<plug>(CybuLastusedNext)")
     end,
