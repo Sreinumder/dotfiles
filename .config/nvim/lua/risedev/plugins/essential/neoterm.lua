39edd20b9b21985e6d67beab83c6d81378b5ad8b
Sreinumder <rojgosai02@gmail.com>
2024-04-24 20:30:04 +0545

    changed keybinds

--- .config/nvim/lua/risedev/plugins/essential/neoterm.lua
+++ .config/nvim/lua/risedev/plugins/essential/neoterm.lua
@@ -1,15 +1,16 @@
 return {
-  'itmecho/neoterm.nvim',
-  config = function()
-    -- Setup global config
-    require('neoterm').setup({
-      clear_on_run = true, -- run clear command before user specified commands
-      mode = 'fullscreen',   -- vertical/horizontal/fullscreen
-      noinsert = false,     -- disable entering insert mode when opening the neoterm window
-      vim.keymap.set({'n', 'c', 't'}, "<A-t><A-t>", "<cmd>NeotermToggle<CR>", {desc = "NeoTermToggle"}),
-      vim.keymap.set('n', "<A-t><A-r>", ":NeotermRun<space>",{desc = "NeoTermToggle"}),
-      vim.keymap.set('n', "<A-t><A-t><A-r>", "<cmd>NeotermRerun<CR>",{desc = "NeoTermToggle"}),
-      vim.keymap.set('n', "<A-t><A-q>", "<cmd>NeotermExit<CR>",{desc = "NeoTermToggle"}),
-    })
-  end,
+	"itmecho/neoterm.nvim",
+	config = function()
+		-- Setup global config
+		require("neoterm").setup({
+			clear_on_run = true, -- run clear command before user specified commands
+			mode = "fullscreen", -- vertical/horizontal/fullscreen
+			noinsert = false, -- disable entering insert mode when opening the neoterm window
+			vim.keymap.set({ "n", "c", "t" }, "<A-t><A-t>", "<cmd>NeotermToggle<CR>", { desc = "NeoTermToggle" }),
+			vim.keymap.set("n", "<A-t><A-r>", ":NeotermRun<space>", { desc = "NeoTermToggle" }),
+			vim.keymap.set("n", "<A-t><A-r><A-l>", ":NeotermRun<space>lazygit<CR>", { desc = "NeoTermToggle" }),
+			vim.keymap.set("n", "<A-t><A-t><A-r>", "<cmd>NeotermRerun<CR>", { desc = "NeoTermToggle" }),
+			vim.keymap.set("n", "<A-t><A-q>", "<cmd>NeotermExit<CR>", { desc = "NeoTermToggle" }),
+		})
+	end,
 }
