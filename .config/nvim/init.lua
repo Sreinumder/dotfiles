95d79e4589ca5348559af1cb9e714bd7d90ff3d5
Sreinumder <rojgosai02@gmail.com>
2024-05-18 12:36:55 +0545

    colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,6 @@
 vim.cmd([[autocmd BufEnter * set formatoptions-=cro]])
 require("risedev.core")
 require("risedev.lazy")
-vim.cmd([[colorscheme everforest]])
 require("risedev.autocmds")
 
 if vim.g.neovide then
