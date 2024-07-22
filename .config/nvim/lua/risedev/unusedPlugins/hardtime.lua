c8dd7ee5178dad3ea33027c5990732098de36fe5
Sreinumder <rojgosai02@gmail.com>
2024-07-22 20:19:32 +0545

    spectre

--- .config/nvim/lua/risedev/unusedPlugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"m4xshen/hardtime.nvim",
-	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	opts = {},
-	config = function()
-		require("hardtime").setup({})
-		vim.keymap.setup("n", "<leader>tth", ":Hardtime toggle<CR>", { desc = "Toggle hardtime" })
-	end,
-}
